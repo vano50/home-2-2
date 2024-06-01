149329d63d863204996d2b9d8e3a753d0bdcdee3
Vano <vano50@mail.ru>
2024-06-01 10:50:36 +0300

    Initial commit

---
+++ home-2-2/home-2-2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+struct accounts {
+    int number;
+    std::string name;
+    double cash;
+};
+
+void changeMoney(accounts& x, double& z) {
+    x.cash = z;
+}
+
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    accounts x;
+    double z;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> x.number;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> x.name;
+    std::cout << "Введите баланс: ";
+    std::cin >> x.cash;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> z;
+
+    changeMoney(x, z);
+
+    std::cout << "Ваш счёт: " << x.name << " " << x.number << " " << x.cash;
+    
+    return 0;
+}
+
